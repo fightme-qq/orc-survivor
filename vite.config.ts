0f4b133f9839b87729dc729c10edb32ada300ae3
Rizvanov Ramazan <rizvanov.r@azurgames.com>
2026-04-13 20:19:04 +0300

    fix: set base path to /dungeon-crawler/ on GitHub Actions build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: process.env.GITHUB_ACTIONS ? '/dungeon-crawler/' : './',
   server: {
     port: 3000,
     host: '127.0.0.1'
